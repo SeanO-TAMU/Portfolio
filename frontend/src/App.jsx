04f15c79b866e1c2dc67d1ffa4e39577e641c5bc
Sean O'Connor <soc1641@icloud.com>
2026-04-06 22:44:30 -0500

    added some global styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
+import Navbar from './components/Navbar.jsx'
+import styles from './App.module.css'
 
 function App() {
   
 
   return (
-     <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <Navbar></Navbar>
+
+      <div className={styles.wrapper}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </>
   )
 }
 
